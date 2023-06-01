4525a74c357965cc1792d9f44139e6bfd23c312a
issam <issambenslimane9999@hotmail.com>
2023-06-01 11:48:36 +0100

    Provide routes to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { AppProvider } from "./providers";
+import { AppRoutes } from "./routes";
 
 function App() {
   return (
     <AppProvider>
-      <h1>hello</h1>
+      <AppRoutes />
     </AppProvider>
   );
 }
